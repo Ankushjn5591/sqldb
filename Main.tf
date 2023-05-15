37332902dd7aceb6266126f69943f77964050974
Ankush Jain <ankushjn38@gmaial.com>
2023-05-15 15:39:58 +0530

    version changed

--- Main.tf
+++ Main.tf
@@ -11,6 +11,7 @@ resource "azurerm_sql_server" "sqlserver" {
   name                         = "mysqlserver5591"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
+  version                      = "15.0"
   administrator_login          = "adminuser"
   administrator_login_password = "P@ssw0rd123!"
 }
